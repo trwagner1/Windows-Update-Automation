c4240d5e7163a95c79de714de036883718e56e26
Ted Wagner <trwagner1@outlook.com>
2018-02-06 14:14:00 -0500

    EOD commit

--- Start-WindowsUpdateAutomation.ps1
+++ Start-WindowsUpdateAutomation.ps1
@@ -1,83 +1,109 @@
 <#
 .SYNOPSIS
-  <Overview of script>
+  This script is used to fully automate the deployment of Windows updates for patching vmware templates/images 
 
 .DESCRIPTION
-  <Brief description of script>
+  For full automation, taking advantage of Microsoft Windows Management Foundation 5.1.  Powershell 3.0 and above required.
+  Installs most recent versions of package provider nuget, and module PSWindowsUpdate.
 
 .PARAMETER <Parameter_Name>
-  <Brief description of parameter input required. Repeat this attribute if required>
+  None
 
 .INPUTS
-  <Inputs if any, otherwise state None>
+  None
 
 .OUTPUTS Log File
   The script log file stored in C:\Windows\Temp\<name>.log
 
 .NOTES
-  Version:        1.0
-  Author:         <Name>
-  Creation Date:  <Date>
-  Purpose/Change: Initial script development
+  Version:			1.0
+  Author:			Ted Wagner
+  Creation Date:	02/06/2018 (initial draft)
+  Purpose/Change:	Initial script development
+  Important:		FIPS must be disabled
+  Requirements:		PowerShell 3.0 and above, run as administrator
+  
+  PSWindowsUpdate Syntax Notes
+  Get default windows update;  if using WSUS or SCCM, those sources are used by default, otherwise will be Microsoft Windows Update
+  Get-WindowsUpdate -AcceptAll -AutoReboot -Confirm:$False
+  
+  Example forces the use of Microsoft Windows Update as a source
+  Get-WindowsUpdate -MicrosoftUpdate
+  
+  Example below limits simultaneous updates to 30
+  Get-WUInstall -MicrosoftUpdate -UpdateCount 30
 
 .EXAMPLE
-  <Example explanation goes here>
-  
-  <Example goes here. Repeat this attribute for more than one example>
+  .\Start-WindowsUpdateAutomation
 #>
 
-<# PSWindowsUpdate Syntax Notes
-Get default windows update, which should be SCCM for computers with SCCM agent installed, otherwise will be Microsoft Windows Update
-Get-WindowsUpdate -AcceptAll -AutoReboot -Confirm:$False
+#Requires -RunAsAdministrator -Version 3.0
 
-Example below goes to Microsoft Windows Update
-Get-WindowsUpdate -MicrosoftUpdate
-
-Example below limits simultaneous updates to 30
-Get-WUInstall -MicrosoftUpdate -UpdateCount 30
-#>
-
-#Requires -RunAsAdministrator
-
-# Inspect modules, install if not available
-If (Get-Module -ListAvailable -Name PSWindowsUpdate){
-	Import-Module PSWindowsUpdate
-}
-Else{
-
-}
-
-
-# Make sure a restart is not required, if so, restart.
-$RStatus = Get-WURebootStatus
-If ($RStatus -NotLike "*not required*"){
-	Restart-Computer
-}
-
-
-
+# Design Notes
+# Step 1: Check FIPS
+# Step 2: Invoke PSSession localhost and install and or update modules.
+# Step 3: Invoke PSSession localhost and run windows updates, accept all, and restart
+# Step 4: If FIPS was enabled, re-enable.
 
 Function Check-FIPSValue {
-	# Future implementation - automate installing the PSWindowsUpdate Module
+	# FIPS check required at this time (February 2018) for proper install of nuget
 	# Note FIPS must be disabled for this to work
-	$FIPSValue = (get-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\FipsAlgorithmPolicy" -name "Enabled").enabled
-	If ($FIPSValue -eq 1){
-		set-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\FipsAlgorithmPolicy" -name "Enabled" -value "0"
+	$Global:FIPSValue = (get-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\FipsAlgorithmPolicy" -name "Enabled").enabled
+	Return $Global:FIPSValue
+}
+
+Function Enable-FIPSValue {
+	set-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\FipsAlgorithmPolicy" -name "Enabled" -value "1"
+}
+
+Function Disable-FIPSValue {
+	set-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\FipsAlgorithmPolicy" -name "Enabled" -value "0"
+}
+
+Function Install-PMandPSWindowsUpdate {
+	# Create Session and install/update NuGet and PSWindowsUpdate
+	$Session = New-PSSession -ComputerName "Localhost"
+	Invoke-Command -Session $Session -ScriptBlock {
+		Import-Module PackageManagement
+		Install-PackageProvider -Name NuGet -Force -Confirm:$False | Out-Null
+		Install-Module -Name PSWindowsUpdate -Force -Confirm:$False | Out-Null
 	}
 }
 
-
-Function Install-PMandPSWindowsUpdate {
-	Import-Module PackageManagement
-	Install-PackageProvider -Name NuGet -Force -Confirm:$False | Out-Null
-	Install-Module -Name PSWindowsUpdate -Force -Confirm:$False | Out-Null
-}
-
 # Check for available updates, if available, then install.  Use Get-WindowsUpdate by itself to get a count.
-
-$WUCount = Get-WindowsUpdate
-If ($WUCount -gt "0"){
-	Get-WindowsUpdate -AcceptAll -AutoReboot -Confirm:$False
+Function Start-InstallAllUpdates {
+	$Session = New-PSSession -ComputerName "Localhost"
+	Invoke-Command -Session $Session -ScriptBlock {
+		Import-Module PSWindowsUpdate
+		$WUCount = Get-WindowsUpdate
+		If ($WUCount -gt "0"){
+			Get-WindowsUpdate -AcceptAll -AutoReboot -Confirm:$False
+		}
+		Else{
+			Write-Host "No Windows Updates to Install"
+			Break
+		}
+	}
 }
 
+# Global variable for FIPS setting
+$Global:FIPSValue = Check-FIPSValue
+
+
+<# Start #>
+# Begin check of FIPS value.  If enabled, disable.
+$Global:FIPSValue = Check-FIPSValue
+If ($Global:FIPSValue -eq "1"){
+	Disable-FIPSValue
+}
+
+<# Perform Windows Updates #>
+
+<# Check FIPS #>
+# If FIPS was enabled at script start, re-enable
+If ($Global:FIPSValue -eq "1"){
+	Enable-FIPSValue
+}
+
+<# End #>
 
