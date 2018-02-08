60d768fd99dfa81b1c3fd2beee582cbc944de023
Ted Wagner <trwagner1@outlook.com>
2018-02-08 08:31:50 -0500

    Update Recent Changes

--- Start-WindowsUpdateAutomation.ps1
+++ Start-WindowsUpdateAutomation.ps1
@@ -3,7 +3,8 @@
   This script is used to fully automate the deployment of Windows updates for patching vmware templates/images 
 
 .DESCRIPTION
-  For full automation, taking advantage of Microsoft Windows Management Foundation 5.1.  Powershell 3.0 and above required.
+  For full automation, taking advantage of Microsoft Windows Management Framework 5.1.  https://www.microsoft.com/en-us/download/details.aspx?id=54616
+  Powershell 3.0 and above required.
   Installs most recent versions of package provider nuget, and module PSWindowsUpdate.
 
 .PARAMETER <Parameter_Name>
@@ -24,13 +25,13 @@
   Requirements:		PowerShell 3.0 and above, run as administrator
   
   PSWindowsUpdate Syntax Notes
-  Get default windows update;  if using WSUS or SCCM, those sources are used by default, otherwise will be Microsoft Windows Update
-  Get-WindowsUpdate -AcceptAll -AutoReboot -Confirm:$False
+  Get default windows update;  if using WSUS or SCCM, those sources are used by default, otherwise will be Microsoft Windows Update; and installs updates
+  Get-WindowsUpdate -AcceptAll -Install -AutoReboot -Confirm:$False
   
-  Example forces the use of Microsoft Windows Update as a source
+  Example forces the use of Microsoft Windows Update as a source; list updates
   Get-WindowsUpdate -MicrosoftUpdate
   
-  Example below limits simultaneous updates to 30
+  Example below limits simultaneous updates to 30; using alias to install updates
   Get-WUInstall -MicrosoftUpdate -UpdateCount 30
 
 .EXAMPLE
@@ -60,6 +61,20 @@ Function Disable-FIPSValue {
 	set-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\FipsAlgorithmPolicy" -name "Enabled" -value "0"
 }
 
+Function Check-WMF{
+    # If PS 3 or 4 prompt to install WMF 5.x
+    If ($PSVersionTable.PSVersion.Major -lt "5" -and $PSVersionTable.PSVersion.Major -ge "3"){
+        Clear-Host
+        Write-Host "This script requires Windows Management Framework 5 or above." -ForegroundColor Yellow; Write-Host "Please install https://www.microsoft.com/en-us/download/details.aspx?id=54616";Write-Host ""
+        $Global:Control = $false
+    }
+    Else{
+        # Not needed with requires statement, insurance
+        Write-Host "This script requires PowerShell 3.0 or above." -ForegroundColor Red;Write-Host ""
+        $Global:Control = $false
+    }
+}
+
 Function Install-PMandPSWindowsUpdate {
 	# Create Session and install/update NuGet and PSWindowsUpdate
 	$Session = New-PSSession -ComputerName "Localhost"
@@ -77,7 +92,7 @@ Function Start-InstallAllUpdates {
 		Import-Module PSWindowsUpdate
 		$WUCount = Get-WindowsUpdate
 		If ($WUCount -gt "0"){
-			Get-WindowsUpdate -AcceptAll -AutoReboot -Confirm:$False
+			Get-WUInstall -AcceptAll -AutoReboot -Confirm:$False
 		}
 		Else{
 			Write-Host "No Windows Updates to Install"
@@ -86,16 +101,29 @@ Function Start-InstallAllUpdates {
 	}
 }
 
-# Global variable for FIPS setting
+# Global variable for run control.
+$Global:Control = $true
+
+# Global variable to save original FIPS setting
 $Global:FIPSValue = Check-FIPSValue
 
 
-<# Start #>
-# Begin check of FIPS value.  If enabled, disable.
-$Global:FIPSValue = Check-FIPSValue
-If ($Global:FIPSValue -eq "1"){
-	Disable-FIPSValue
+<# Check PS Version for WMF version compatibility #>
+Check-WMF
+
+<# Check FIPS.  If enabled, disable. #>
+If ($Global:Control){
+    $Global:FIPSValue = Check-FIPSValue
+    If ($Global:FIPSValue -eq "1"){
+	    Disable-FIPSValue
+    }
 }
+
+<# Install and update modules #>
+If ($Global:Control){
+    Install-PMandPSWindowsUpdate
+}
+
 
 <# Perform Windows Updates #>
 
